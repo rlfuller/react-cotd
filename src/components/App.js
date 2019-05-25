4ce7f3fca4ca2e107886e8402c84e7579d68b1ec
Rachel Fuller <rlfuller@gmail.com>
2019-05-25 16:28:03 -0400

    order component

--- src/components/App.js
+++ src/components/App.js
@@ -53,7 +53,8 @@ class App extends React.Component {
                         ))}
                     </ul>
                 </div>
-                <Order></Order>
+                <Order fishes={this.state.fishes} order={this.state.order}>
+                </Order>
                 <Inventory 
                     addFish={this.addFish}
                     loadSampleFishes={this.loadSampleFishes}
